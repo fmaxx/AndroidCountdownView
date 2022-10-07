bee5fc3504647705d022d1afeda2288c2918b10c
Maxim Firsov <firsoffmaxim@gmail.com>
2022-10-07 12:44:15 +0500

    Add first set up files and configurations

---
+++ core/src/main/java/com/github/fmaxx/androidCountdownView/core/Test.kt
@@ -0,0 +1,11 @@
+package com.github.fmaxx.androidCountdownView.core
+
+/**
+ * Created by Maxim Firsov on 07.10.2022.
+ * firsoffmaxim@gmail.com
+ */
+class Test {
+    fun test(): Boolean {
+        return true
+    }
+}
---
+++ core/src/test/java/com/github/fmaxx/androidCountdownView/core/TestTest.kt
@@ -0,0 +1,17 @@
+package com.github.fmaxx.androidCountdownView.core
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+
+/**
+ * Created by Maxim Firsov on 07.10.2022.
+ * firsoffmaxim@gmail.com
+ */
+internal class TestTest {
+
+    @Test
+    fun test1() {
+        assertTrue(com.github.fmaxx.androidCountdownView.core.Test().test())
+    }
+}
